275c641d68526091511c21e970ba0730c0e2db51
Jessie Yang <fenyang@thoughtworks.com>
2018-04-08 23:49:39 +0800

    [jessie] add react-native-image-picker to choose image

--- src/pictureList.js
+++ src/pictureList.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import ImagePicker, { showImagePicker } from "react-native-image-picker";
 
 const styles = StyleSheet.create({
   container: {
@@ -9,15 +10,39 @@ const styles = StyleSheet.create({
   }
 })
 
+const options = {
+  title: 'Use your own photo',
+  storageOptions: {
+    skipBackup: true,
+    path: 'images',
+  }
+}
+
 export default class PictureList extends React.Component {
   goHowOld = () => {
     this.props.navigation.navigate('HowOld')
+  }
+
+  showImagePicker = () => {
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+    
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else {
+        // TODO: need to check if we send the data or uri to howOld page
+      }
+    });
   }
 
   render() {
     return (
       <View style={styles.container}>
         <Text onPress={this.goHowOld}>Press me to go to HowOld page.</Text>
+        
+        <Button onPress={this.showImagePicker} title="Use your own photo" />
       </View>
     )
   }
