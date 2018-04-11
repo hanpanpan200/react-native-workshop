11f85fd72144362297b51bedc9acc7de02cf76c6
Dan Chen <dchen@thoughtworks.com>
2018-04-11 21:03:13 +0800

    [Dan] apply siper

--- src/pictureList.js
+++ src/pictureList.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 import ImagePicker, { showImagePicker } from "react-native-image-picker";
+import Swiper from 'react-native-swiper';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB',
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5',
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
   }
 })
 
@@ -26,7 +50,7 @@ export default class PictureList extends React.Component {
   showImagePicker = () => {
     ImagePicker.showImagePicker(options, (response) => {
       console.log('Response = ', response);
-    
+
       if (response.didCancel) {
         console.log('User cancelled image picker');
       } else if (response.error) {
@@ -41,7 +65,18 @@ export default class PictureList extends React.Component {
     return (
       <View style={styles.container}>
         <Text onPress={this.goHowOld}>Press me to go to HowOld page.</Text>
-        
+        <Swiper style={styles.wrapper} showsButtons={true}>
+          <View style={styles.slide1}>
+            <Text style={styles.text}>Hello Swiper</Text>
+          </View>
+          <View style={styles.slide2}>
+            <Text style={styles.text}>Beautiful</Text>
+          </View>
+          <View style={styles.slide3}>
+            <Text style={styles.text}>And simple</Text>
+          </View>
+        </Swiper>
+
         <Button onPress={this.showImagePicker} title="Use your own photo" />
       </View>
     )
