4f9def016edbfd0c7da79ec5d6a0e23fda7bee5c
zhaolan <lanzhao@thoughtworks.com>
2018-05-10 21:15:42 +0800

    zhaolan add style for age info

--- src/ageInfo.js
+++ src/ageInfo.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { GENDER_MAP } from './constants/constant'
+import { COLOR } from "./constants/styleGuide";
 
 const styles = StyleSheet.create({
   container: {
@@ -8,20 +9,45 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     position: 'absolute',
-    borderColor: '#f2c034',
     borderWidth: 3,
+    borderColor: COLOR.RED,
     zIndex: 100,
   },
   textStyle: {
     fontSize: 12,
-    color: '#fd7039',
     fontWeight: 'bold',
-  }
+    textAlign: 'center',
+    color: COLOR.WHITE,
+  },
+  infoContainer: {
+    position: 'absolute',
+    alignItems: 'center',
+    padding: 8,
+    borderRadius: 4,
+    backgroundColor: COLOR.RED,
+  },
+  triangle: {
+    position: 'absolute',
+    bottom: -9,
+    width: 0,
+    height: 0,
+    borderStyle: 'solid',
+    borderLeftWidth: 5,
+    borderRightWidth: 5,
+    borderTopWidth: 10,
+    backgroundColor: COLOR.TRANSPARENT,
+    borderLeftColor: COLOR.TRANSPARENT,
+    borderRightColor: COLOR.TRANSPARENT,
+    borderTopColor: COLOR.RED,
+  },
 });
 
 const AgeInfo = ({ age, infoStyle, gender }) => (
   <View style={[styles.container, infoStyle]}>
-    <Text style={styles.textStyle}>{GENDER_MAP[gender]} {age}岁</Text>
+    <View style={[styles.infoContainer, {bottom: infoStyle.height + 5}]}>
+      <Text style={styles.textStyle}>{GENDER_MAP[gender]} {age}岁</Text>
+      <View style={styles.triangle}></View>
+    </View>
   </View>
 );
 
--- src/constants/styleGuide.js
+++ src/constants/styleGuide.js
@@ -2,4 +2,5 @@ export const COLOR = {
   BLUE: '#048BA8',
   WHITE: '#FFF',
   TRANSPARENT: 'transparent',
+  RED: "#339999",
 }
